ba5218dc836e793cf0a61fcf0e021b3ef0e3652a
QuesadaHerreraGonzalo <quesada_gonzalo@live.com>
2017-05-02 18:57:32 -0300

    Segunda actualizacion del menu de inicio (agregadas las opciones del menu principal).

--- menu_de_inicio.py
+++ menu_de_inicio.py
@@ -1,7 +1,14 @@
 def mostrarMenuDeInicio():
     print(" ☻ ☻ ☻ ¡¡Bienvenido a Lights Out!! ☻ ☻ ☻ ")
-    modoElegido = input("¿Que modo desea jugar?  +Aleatorio+ (A) o -Predeterminado- (P) O presione (S) para salir: ")
+    modoElegido = input("¿Que modo desea jugar?  +Aleatorio+ (A), -Predeterminado- (P) o presione (X) para salir: ")
+
     print(modoElegido)
 
+    modoElegido = modoElegido.lower()
 
-
+    if (modoElegido == "a"):
+        print("+¡Modo Aleatorio seleccionado!+")
+    elif (modoElegido == "p"):
+        print ("-¡Modo Predeterminado seleccionado!-")
+    elif (modoElegido == "x"):
+        print ("Selecciono salir :(")
